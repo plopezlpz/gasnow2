8cfb6027ede992ebcd4dd5e3eb071bdd03478b8d
pablo <p.lopez.lpz@gmail.com>
2021-10-19 14:07:23 +0300

    subscriptions

--- cmd/api/handler/ws.go
+++ cmd/api/handler/ws.go
@@ -2,15 +2,13 @@ package handler
 
 import (
 	"context"
-	"fmt"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
-	"github.com/pkg/errors"
 	"github.com/plopezlpz/gasnow2/coingecko"
 	"github.com/plopezlpz/gasnow2/gas"
-	"golang.org/x/sync/errgroup"
 )
 
 type WS struct {
@@ -32,71 +30,104 @@ func (w WS) Subscribe(c echo.Context) error {
 	}
 	defer ws.Close()
 
-	g, ctx := errgroup.WithContext(c.Request().Context())
-	g.Go(func() error {
-		return w.subscribeToGasPrice(ctx, c.Logger(), ws)
-	})
-	g.Go(func() error {
-		return w.subscribeToCurrencyPrice(ctx, c.Logger(), ws)
-	})
-	if err := g.Wait(); err != nil {
-		c.Logger().Error(err)
-	}
+	ctx, cancel := context.WithCancel(c.Request().Context())
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	gasPrice := w.subscribeToGasPrice(ctx, c.Logger(), &wg)
+	currencyPrice := w.subscribeToCurrencyPrice(ctx, c.Logger(), &wg)
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case gp := <-gasPrice:
+				if err := ws.WriteJSON(gp); err != nil {
+					c.Logger().Error(err)
+					cancel()
+					return
+				}
+			case cp := <-currencyPrice:
+				if err := ws.WriteJSON(cp); err != nil {
+					c.Logger().Error(err)
+					cancel()
+					return
+				}
+			}
+		}
+	}()
+	wg.Wait()
 	return nil
 }
 
-func (w WS) subscribeToGasPrice(ctx context.Context, logger echo.Logger, ws *websocket.Conn) error {
-	for {
-		// Check if any error occurred in any other gorouties:
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("stopping %v", "gaspriceSubscription")
-		default: // to avoid blocking
-		}
-		gasPrices, err := w.gasServer.GetGasPrice()
-		if err != nil {
-			logger.Error(err)
+func (w WS) subscribeToGasPrice(ctx context.Context, logger echo.Logger, wg *sync.WaitGroup) <-chan RespGas {
+	out := make(chan RespGas)
+	var lastUpdate int64 = 0
+
+	go func() {
+		defer wg.Done()
+		defer close(out)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default: // avoid blocking
+			}
+			gasPrices, err := w.gasServer.GetGasPrice()
+			if err != nil {
+				logger.Error(err)
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			if gasPrices.Timestamp == lastUpdate {
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			lastUpdate = gasPrices.Timestamp
+			out <- RespGas{
+				Type: "gasprice",
+				Data: gasPrices,
+			}
 			time.Sleep(5 * time.Second)
-			continue
 		}
-		// Write
-		if err := ws.WriteJSON(RespGas{
-			Type: "gasprice",
-			Data: gasPrices,
-		}); err != nil {
-			return errors.Wrap(err, "connection closed")
-		}
-		time.Sleep(5 * time.Second)
-	}
+	}()
+	return out
+
 }
 
-func (w WS) subscribeToCurrencyPrice(ctx context.Context, logger echo.Logger, ws *websocket.Conn) error {
+func (w WS) subscribeToCurrencyPrice(ctx context.Context, logger echo.Logger, wg *sync.WaitGroup) <-chan RespCurrency {
+	out := make(chan RespCurrency)
 	var lastUpdate int64 = 0
-	for {
-		// Check if any error occurred in any other gorouties:
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("stopping %v", "currencyPriceSubscription")
-		default: // to avoid blocking
-		}
-		currencyPrice, err := w.gasServer.GetCurrencyPrice("ethereum", "usd")
-		if err != nil {
-			return errors.Wrap(err, "getting currency price")
-		}
-		if currencyPrice.LastUpdatedAt == lastUpdate {
+
+	go func() {
+		defer wg.Done()
+		defer close(out)
+		for {
+			// Check if any error occurred in any other gorouties:
+			select {
+			case <-ctx.Done():
+				return
+			default: // avoid blocking
+			}
+			currencyPrice, err := w.gasServer.GetCurrencyPrice("ethereum", "usd")
+			if err != nil {
+				logger.Error(err)
+				close(out)
+				return
+			}
+			if currencyPrice.LastUpdatedAt == lastUpdate {
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			lastUpdate = currencyPrice.LastUpdatedAt
+			out <- RespCurrency{
+				Type: "currencyprice",
+				Data: currencyPrice,
+			}
 			time.Sleep(5 * time.Second)
-			continue
 		}
-		lastUpdate = currencyPrice.LastUpdatedAt
-		// Write
-		if err := ws.WriteJSON(RespCurrency{
-			Type: "currencyprice",
-			Data: currencyPrice,
-		}); err != nil {
-			return errors.Wrap(err, "connection closed")
-		}
-		time.Sleep(5 * time.Second)
-	}
+	}()
+	return out
 }
 
 type RespGas struct {
